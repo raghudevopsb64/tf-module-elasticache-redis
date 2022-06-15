e2b6baa3ac41152416dcd0b2724e58c1aae927f7
Raghu K <raghuk.vit@gmail.com>
2022-06-15 08:59:51 +0530

    Update roboshop-infra/all-modules/tf-module-elasticache-redis/

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "elastic" {
   zone_id = var.PRIVATE_HOSTED_ZONE_ID
-  name    = "redis-${var.ENV}.roboshop.internal"
+  name    = "${var.COMPONENT}-redis-${var.ENV}.roboshop.internal"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_elasticache_cluster.escluster.cache_nodes[0].address]
