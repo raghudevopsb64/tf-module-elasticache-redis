d7272224e0d27d583d9b87218899abd682744f95
Raghu K <raghuk.vit@gmail.com>
2022-06-15 08:44:23 +0530

    Update roboshop-infra/all-modules/tf-module-elasticache-redis/

--- main.tf
+++ main.tf
@@ -7,4 +7,5 @@ resource "aws_elasticache_cluster" "escluster" {
   engine_version       = var.ENGINE_VERSION
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.main.name
+  security_group_ids   = [aws_security_group.main.id]
 }
